f1ac840292454816f13640a0a835530fea6e1630
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-05-28 20:28:25 -0300

    :sparkles: home/start screen

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { StartScreen } from './components/StartScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <StartScreen />
+    </div>
+  );
+}
+
+export default App;
