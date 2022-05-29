54f6a1676697f01fb996e1fb58d3e5334347895d
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-05-28 21:25:50 -0300

    :sparkles: picked word and category

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,34 @@ import { stages, wordsList } from './data/words';
 export const App = () => {
 
   const [start, game, end] = stages;
-
   const [gameStage, setGameStage] = useState(start.name);
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState('');
 
   const [words] = useState(wordsList);
 
+  const pickedWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * categories.length)];
+
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return { word, category };
+  }
+
   const startGame = () => {
+    const { word, category } = pickedWordAndCategory();
+
+    const wordLetters = String(word)
+      .split('')
+      .map(
+        letter => letter.toLowerCase()
+      );
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
     setGameStage(game.name);
   }
 
